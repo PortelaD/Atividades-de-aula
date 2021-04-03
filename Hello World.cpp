aae60df8295bc80868d0f4643ea2d3fb249255c6
PortelaD <79917387+PortelaD@users.noreply.github.com>
2021-04-03 09:03:11 -0300

    Add files via upload

---
+++ Hello World.cpp
@@ -0,0 +1,14 @@
+// Essas barras são sempre comentário
+// printf= comando para escrever na tela 
+// Estrutura básica do algorítmo
+// (\n)quebra de linha
+// Case sensitive: diferenciação entre maiúsculas e minúsculas
+#include<stdio.h> // "Biblioteca" para que entenda o comando int main
+// "Biblioteca" para aceitar acento, locdale.h
+int main(){ // Início do algoritmo
+printf("Hello World!");	
+return 0; // Retorno da função main
+} //Final do algoritmo
+
+
+
